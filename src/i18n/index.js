5e584efb972cc22e463cedcb4847a57b91f01c3a
xiongwei <>
2022-09-02 17:41:40 +0800

    chores: i18n && code clean

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -14,12 +14,18 @@ const resources = {
       framework: 'Framework',
       search: 'Search',
       reset: 'Reset',
+      download: 'Download',
+      update: 'Update',
+      create: 'Create',
       name: 'Name',
       select: 'select',
       'search component': 'search component',
       'components built with Bamboo': 'components built with Bamboo',
       description:
-        'Build your web page more faster with premade responsive components designed and built by Bamboo.'
+        'Build your web page more faster with premade responsive components designed and built by Bamboo.',
+      'give-me-star': 'Give this project a star to help it doing better~',
+      'use this component': 'use this component',
+      'have-fun': "Hoping y'll having fun with it."
     }
   },
   zh_CN: {
@@ -27,16 +33,21 @@ const resources = {
       document: '文档',
       hot: '热门',
       recent: '最新',
-      framework: '框架',
+      framework: '框架类型',
       search: '搜索',
       reset: '重置',
       name: '组件名称',
       select: '请选择',
+      update: '更新时间',
+      create: '创建时间',
+      download: '下载次数',
       'search component': '组件搜索',
-      'Welcome to React': '阿斯顿发',
       'components built with Bamboo': '个组件可使用',
       description:
-        '直接使用由 Bamboo 构建出来的一系列物料组件，帮助你在开发前端页面的时候更快速、效率更高。'
+        '直接使用由 Bamboo 构建出来的一系列物料组件，帮助你在开发前端页面的时候更快速、效率更高。',
+      'give-me-star': '给此项目点个star帮助其做的更好~',
+      'use this component': '使用这个组件',
+      'have-fun': '希望你能玩的开心'
     }
   }
 }
