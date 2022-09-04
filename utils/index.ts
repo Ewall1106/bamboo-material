0f64830dec978caf939c31cb1f72400d476129b9
danielxiong <ewall1106@gmail.com>
2022-09-04 12:13:23 +0800

    feat: Header migrate

---
+++ utils/index.ts
@@ -0,0 +1,35 @@
+import dayjs from 'dayjs'
+import queryString from 'query-string'
+
+export const pageTypeList = ['react', 'vue', 'vue-next', 'wx-mini', 'taro', 'react-native']
+
+export const groupUIList = ['basic', 'vant', 'element', 'antd', 'antm', 'naive']
+
+export const getTagColor = s => {
+  switch (s) {
+    case 'react':
+      return 'blue'
+    case 'vue':
+      return 'lime'
+    case 'vue-next':
+      return 'green'
+    case 'antd':
+      return '#1890ff'
+    case 'element':
+      return '#1989fa'
+    case 'vant':
+      return '#0094ff'
+    default:
+      break
+  }
+}
+
+export const getTime = time => {
+  return dayjs(time).format('YYYY-MM-DD')
+}
+
+export const getLng = () => {
+  if (typeof window !== 'undefined') {
+    return window.localStorage.getItem('lng')
+  }
+}
