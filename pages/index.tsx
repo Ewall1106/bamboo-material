357967f794c13896c4ff98a60fd627fa1eea45f8
danielxiong <ewall1106@gmail.com>
2022-09-04 14:26:42 +0800

    config: next-i18next

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,33 @@
-import Home from './home'
+import { GetStaticProps } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 import type { NextPage } from 'next'
 
 const Index: NextPage = () => {
-  return <Home />
+  const router = useRouter()
+  const { t } = useTranslation('common')
+
+  const changeTo = router.locale === 'en' ? 'de' : 'en'
+
+  return (
+    <>
+      <p>{t('title')}</p>
+      <Link href="/" locale={changeTo}>
+        <button>{t('change-locale', { changeTo })}</button>
+      </Link>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async ({ locale }: any) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common']))
+    }
+  }
 }
 
 export default Index
