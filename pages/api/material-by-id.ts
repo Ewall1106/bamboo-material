113c23b1ca2cb57b682c056f833f7969aa56dc72
xiongwei <>
2022-09-06 17:30:20 +0800

    feat: api req.query validate

--- pages/api/material-by-id.ts
+++ pages/api/material-by-id.ts
@@ -5,9 +5,13 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 export const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { db } = await connectToDatabase()
-  const query: any = req.query
+  const { id }: any = req.query
 
-  const result = await db.collection('materials').findOne({ _id: new ObjectId(query.id) })
+  if (!id) {
+    return res.status(500).json({ msg: 'id is necessary' })
+  }
+
+  const result = await db.collection('materials').findOne({ _id: new ObjectId(id) })
 
   return res.status(200).json(result)
 }
